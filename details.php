4aa30b490a1a6f2a4cc22bbc35baf4b221155564
korkmazfb <32840@ma-web.nl>
2022-06-03 13:46:08 +0200

    les 3

---
+++ details.php
@@ -0,0 +1,49 @@
+<?php
+require 'functions.php';
+$connection = dbConnect();
+
+if( !isset($_GET['id']) ){
+    echo "de ID is niet gezet";
+    exit;
+}
+
+$id = $_GET['id'];
+$check_int = filter_var($id, FILTER_VALIDATE_INT);
+if($check_int == false){
+    echo "dit is geen getal (integer)";
+    exit;
+}
+
+$statement = $connection->prepare('SELECT * FROM `projecten` WHERE id=?');
+$params = [$id];
+$statement->execute($params);
+$place = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="header"><?php echo $place['titel']?></h1>
+        <figure><img src="img/<?php echo $place['foto']?>" alt="" width="700px" height="500px"></figure>
+        <p class="p"><?php echo $place['info']?></p>
+
+        
+
+     
+
+        
+
+    </div>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -19,38 +19,23 @@ $result = $connection->query('SELECT * FROM `projecten`');
 </head>
 <body>
     <div class="container">
-        <h1>dit zijn al mijn projecten!</h1>
+        <h1>portfolio</h1>
 
         <section class="projecten">
             <?php foreach($result as $row): ?>
+
             <article class="titels">
                 <h2><?php echo $row['titel']; ?></h2>
-                <figure class="foto"><img src="img/<?php echo $row['foto']; ?>" alt="project 1"></figure>
+                <figure class="foto"><img src="img/<?php echo $row['foto']; ?>" alt="project 1" width="300px" height="300px"></figure>
                 <header>
                 </header>
-                <p><?php echo $row['beschrijving']; ?></p>
+                <p class="beschrijving"><?php echo $row['beschrijving']; ?></p>
+                <a class="button" href="details.php?id=<?php echo $row['id'];?>">Meer Info</a>
             </article>
+
             <?php endforeach; ?>
 
-                <!-- <article class="titels">
-                    <h2>project</h2>
-                    <figure class="foto"><img src="img/project2.jpg" alt="project 2"></figure>
-                    <header>
-                        <h3>project 1</h3>
-                    </header>
-                    <p>mijn tweede project</p>
-                </article>
-
-                    <article class="titels">
-                        <h2>project</h2>
-                        <figure class="foto"><img src="img/project3.jpg" alt="project 3"></figure>
-                        <header>
-                            <h3>project 1</h3>
-                        </header>
-                        <p>mijn derde project</p>
-                    </article>
- -->
-
+        
 
 
         </section>
